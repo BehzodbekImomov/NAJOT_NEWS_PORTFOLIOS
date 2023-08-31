d79a399a6085de10770f22bb6fabcfa2fd427004
mr <behzodbeki65@gmail.com>
2023-08-31 13:14:59 +0500

    commit-6

--- src/pages/Admin/CategoryP.jsx
+++ src/pages/Admin/CategoryP.jsx
@@ -24,6 +24,7 @@ import {
 import { getImage } from "../../utils/getImage";
 import { PER_PAGE } from "../../constants";
 import { request } from "../../server/request";
+// import { request } from "../../server/request";
 
 const { useForm } = Form;
 const { TextArea } = Input;
